aafdde959fbfd75ef9bf631c25eb76f509243745
alvathunder <87148598+alvathunder@users.noreply.github.com>
2023-12-05 10:55:50 +0100

    Update envtest.py

--- envtest.py
+++ envtest.py
@@ -1,10 +1,10 @@
+#THIS IS A TEST TO MAKE SURE THAT THE ENV VARS CONTAIN THE RIGHT DATA
+
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from the .env file
 load_dotenv()
 
-# Print loaded variables for debugging
 print(os.getenv("HOST"))
 print(os.getenv("PORT"))
 print(os.getenv("USER_SQL"))
